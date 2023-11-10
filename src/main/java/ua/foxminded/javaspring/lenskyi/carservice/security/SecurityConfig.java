78ae023130504960fcd2096598f893e057a95593
Denys Lenskyi <denislensky94@gmail.com>
2023-11-10 18:59:03 -0500

    Get requests opened for all, POST,PUT,DELETE only for authenticated users. Postman collection updated

--- src/main/java/ua/foxminded/javaspring/lenskyi/carservice/security/SecurityConfig.java
+++ src/main/java/ua/foxminded/javaspring/lenskyi/carservice/security/SecurityConfig.java
@@ -3,7 +3,7 @@ package ua.foxminded.javaspring.lenskyi.carservice.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,7 +27,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/v1/model/***").permitAll()
+                        .requestMatchers(HttpMethod.GET).permitAll()
+                        .requestMatchers(HttpMethod.DELETE).authenticated()
+                        .requestMatchers(HttpMethod.PUT).authenticated()
+                        .requestMatchers(HttpMethod.POST).authenticated()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder())));
@@ -43,26 +46,4 @@ public class SecurityConfig {
         jwtDecoder.setJwtValidator(validator);
         return jwtDecoder;
     }
-
-    /*
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(requests -> requests
-                        .requestMatchers(LOGIN_URL, "/login/readme", "/css/**", "/webjars/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin(form -> form
-                        .loginPage(LOGIN_URL)
-                        .permitAll()
-                        .defaultSuccessUrl("/home", true)
-                )
-                .logout(logout -> logout
-                        .permitAll()
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                        .logoutSuccessUrl(LOGIN_URL));
-
-        return http.build();
-    }
-     */
 }
